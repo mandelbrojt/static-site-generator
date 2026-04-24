f87f731c157a58534d4b5be4629043b5d762e234
mandelbrojt <luis.moreno@exatec.tec.mx>
2026-04-24 02:17:08 +0000

    Add a sample html node and print its representation

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from textnode import TextNode
+from htmlnode import HTMLNode
 
 def main():
     text_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    html_node = HTMLNode("h1", "Our Blog is the Best", [], {"href": "https://www.google.com", "target": "_blank"})
     print(text_node)
+    print(html_node)
 
 
 if __name__ == "__main__":
