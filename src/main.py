8e2bf97eee9fb5f091d12b386386e442931c10d0
mandelbrojt <luis.moreno@exatec.tec.mx>
2026-05-31 06:22:48 +0000

    Implement CH5 -> L1

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
-from textnode import TextNode
-from htmlnode import HTMLNode
+import os
+import shutil
 
-def main():
-    text_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
-    html_node = HTMLNode("h1", "Our Blog is the Best", [], {"href": "https://www.google.com", "target": "_blank"})
-    print(text_node)
-    print(html_node)
+from copystatic import copy_files_recursive
 
 
-if __name__ == "__main__":
-    main()
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def main() -> None:
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
+main()
+
