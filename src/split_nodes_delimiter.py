ca29c106cb69afd85ed1d656cb0062483c59aebd
mandelbrojt <luis.moreno@exatec.tec.mx>
2026-05-31 01:30:48 +0000

    Add helper function to split text nodes by a given delimiter

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -1,13 +1,36 @@
 from textnode import TextNode, TextType
 
 
-def filter_nodes(nodes, operation, node_text_type):
-    if operation == "eq":
-        return list(filter(lambda x: x.text_type == node_text_type))
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes: list = []
+    
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            splitted_node_parts: list = node.text.split(delimiter)
+            if len(splitted_node_parts) % 2 == 0:
+                raise ValueError(f"'{delimiter}' delimiter is not valid Markdown syntax.")
+            # Process splitted parts...
+            for i in range(len(splitted_node_parts)):
+                # ignore empty strings
+                if splitted_node_parts[i] == "":
+                    continue
+                # even indices are always standard text
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(splitted_node_parts[i], TextType.TEXT))
+                # odd indices are always the formatted text type
+                elif i % 2 != 0:
+                    new_nodes.append(TextNode(splitted_node_parts[i], text_type))
+
+    return new_nodes
 
 
+sample_nodes = [
+    TextNode("This is text with a `code block` word", TextType.TEXT),
+    TextNode("This is a text with **booled phrase**", TextType.TEXT),
+    TextNode("This is a text with a __italic phrase__", TextType.TEXT),
+]
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    text_nodes = list(filter(lambda x: x.text_type == TextType.TEXT))
-    new_nodes.extend()
+print(split_nodes_delimiter(sample_nodes, "`", TextType.CODE))
+
